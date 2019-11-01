1f10617c302ab9e957ca6372b4998f973a6baeb6
Geethu Padachery <geethu@Geethus-MacBook-Pro.local>
2019-11-01 14:50:36 -0700

    Initial commit

---
+++ src/main/java/com/sjsu/jenkins/src/main/java/com/sjsu/esp/jenkins/App.java
@@ -0,0 +1,14 @@
+package com.sjsu.esp.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {	System.out.println("Enterprise Software Platform");
+        System.out.println( "Integrating Jenkins with Github" );
+        System.out.println("test");
+    }
+}
